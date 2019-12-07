91ab79182c6d13c583dcef0aa5b85768c3a45d3d
Amir Ismail <eng.amir.ismail@gmai.com>
2019-12-07 06:06:40 +0300

    project share button

---
+++ TeamEvaluation.DAL/Entities/Judge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeamEvaluation.DAL.Entities
+{
+    public class Judge
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Project> Projects { get; set; }
+
+
+    }
+}
